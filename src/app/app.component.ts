45273cb446e6947fbe380d8572a05f474483ccc8
Shinkei <shinkeygentatsu@yahoo.com.mx>
2017-06-05 20:01:24 -0500

    agregar un componente y event binding y property binding y expressions y la mision de la parte final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'aplicacion proyecto 0';
+  author = 'Shinkei';
+  count = 0;
+  imageSrc = './../favicon.ico';
+  enableInput = true;
 }
